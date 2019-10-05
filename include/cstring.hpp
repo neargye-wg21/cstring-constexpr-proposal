36dd872ee99bd10e0c6fde738ecb9d805e6b4430
neargye <neargye@gmail.com>
2019-10-05 19:51:21 +0500

    fix clang

--- include/cstring.hpp
+++ include/cstring.hpp
@@ -6,7 +6,9 @@
 namespace proposal {
 
 constexpr std::size_t strlen(const char* str) noexcept {
-#if defined(__GNUC__)
+#if defined(__clang__) || defined(_MSC_VER)
+  return __builtin_strlen(str);
+#elif defined(__GNUC__)
   // gcc-9 error: __builtin_strlen is not a constant expression!
   if (std::is_constant_evaluated()) {
     std::size_t i = 0;
@@ -17,8 +19,6 @@ constexpr std::size_t strlen(const char* str) noexcept {
   } else {
     return std::strlen(str);
   }
-#elif defined(_MSC_VER)
-  return __builtin_strlen(str);
 #endif
 }
 
@@ -30,6 +30,14 @@ constexpr char* strncpy(char* dest, const char* src, std::size_t count) noexcept
   } else {
     return std::strncpy(dest, src, count);
   }
+#elif defined(__clang__) || defined(_MSC_VER)
+  for (std::size_t i = 0; i < count; ++i) {
+    if (src[i] == '\0') {
+      break;
+    }
+    dest[i] = src[i];
+  }
+  return dest;
 #elif defined(__GNUC__)
   if (std::is_constant_evaluated()) {
     for (std::size_t i = 0; i < count; ++i) {
@@ -42,14 +50,6 @@ constexpr char* strncpy(char* dest, const char* src, std::size_t count) noexcept
   } else {
     return std::strncpy(dest, src, count);
   }
-#elif defined(_MSC_VER)
-  for (std::size_t i = 0; i < count; ++i) {
-    if (src[i] == '\0') {
-      break;
-    }
-    dest[i] = src[i];
-  }
-  return dest;
 #endif
 }
 
@@ -75,7 +75,19 @@ constexpr int strcmp(const char* lhs, const char* rhs) noexcept {
   const std::size_t lhs_len = strlen(lhs);
   const std::size_t rhs_len = strlen(rhs);
   const std::size_t len = std::min(lhs_len, rhs_len);
-#if defined(__GNUC__)
+#if defined(__clang__) || defined(_MSC_VER)
+  if (const int ans = __builtin_memcmp(lhs, rhs, len); ans != 0) {
+    return ans;
+  }
+
+  if (lhs_len < rhs_len) {
+    return -1;
+  } else if (lhs_len > rhs_len) {
+    return 1;
+  }
+
+  return 0;
+#elif defined(__GNUC__)
   if (std::is_constant_evaluated()) {
     for (std::size_t i = 0; i < len; ++i) {
       if (lhs[i] < rhs[i]) {
@@ -95,23 +107,14 @@ constexpr int strcmp(const char* lhs, const char* rhs) noexcept {
   } else {
     return std::strcmp(lhs, rhs);
   }
-#elif defined(_MSC_VER)
-  if (const int ans = __builtin_memcmp(lhs, rhs, len); ans != 0) {
-    return ans;
-  }
-
-  if (lhs_len < rhs_len) {
-    return -1;
-  } else if (lhs_len > rhs_len) {
-    return 1;
-  }
-
-  return 0;
 #endif
 }
 
 constexpr int strncmp(const char* lhs, const char* rhs, std::size_t count) noexcept {
-#if defined(__GNUC__)
+#if defined(__clang__) || defined(_MSC_VER)
+  return __builtin_memcmp(lhs, rhs, count);
+#elif defined(__GNUC__)
+// gcc-9 error: __builtin_memcmp is not a constant expression!
   if (std::is_constant_evaluated()) {
     for (std::size_t i = 0; i < count; ++i) {
       if (lhs[i] < rhs[i]) {
@@ -125,8 +128,6 @@ constexpr int strncmp(const char* lhs, const char* rhs, std::size_t count) noexc
   } else {
     return std::strncmp(lhs, rhs, count);
   }
-#elif defined(_MSC_VER)
-  return __builtin_memcmp(lhs, rhs, count);
 #endif
 }
 
