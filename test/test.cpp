664ba3e6da9161e99fc4613f518aeee31f5fa01e
neargye <neargye@gmail.com>
2019-10-05 19:25:28 +0500

    add more test

--- test/test.cpp
+++ test/test.cpp
@@ -45,12 +45,61 @@ TEST_CASE("strlen") {
 TEST_CASE("strcpy") {
   constexpr std::array<char, 7> char_array = {'1', '2', '3', '4', '5', '6', '\0'};
   auto test_strcpy = [](const char* str) constexpr {
-    std::array<char, 7> m{};
+    std::array<char, 32> m = {'\0'};
     proposal::strcpy(m.data(), str);
     return m;
   };
 
   constexpr auto char_array_cpy = test_strcpy(char_array.data());
-  //static_assert(char_array_cpy == char_array);
-  REQUIRE(char_array_cpy == char_array);
+  static_assert(proposal::strcmp(char_array_cpy.data(), char_array.data()) == 0);
+  REQUIRE(std::strcmp(char_array_cpy.data(), char_array.data()) == 0);
+}
+
+TEST_CASE("strncpy") {
+  constexpr std::array<char, 7> char_array = {'1', '2', '3', '4', '5', '6', '\0'};
+  constexpr auto n = 5;
+  auto test_strncpy = [](const char* str, std::size_t n) constexpr {
+    std::array<char, 32> m = {'\0'};
+    proposal::strncpy(m.data(), str, n);
+    return m;
+  };
+
+  constexpr auto char_array_cpy = test_strncpy(char_array.data(), n);
+  static_assert(proposal::strncmp(char_array_cpy.data(), char_array.data(), n) == 0);
+  REQUIRE(std::strncmp(char_array_cpy.data(), char_array.data(), n) == 0);
+}
+
+TEST_CASE("strcat") {
+}
+
+TEST_CASE("strncat") {
+}
+
+TEST_CASE("strcmp") {
+  constexpr char str1[] = "Hello, world!";
+  constexpr char str2[] = "Hello, everybody!";
+  constexpr char str3[] = "Hello, world!";
+
+  static_assert(proposal::strcmp(str1, str2) > 0);
+  REQUIRE(proposal::strcmp(str1, str2) > 0);
+
+  static_assert(proposal::strcmp(str2, str3) < 0);
+  REQUIRE(proposal::strcmp(str2, str3) < 0);
+
+  static_assert(proposal::strcmp(str1, str3) == 0);
+  REQUIRE(proposal::strcmp(str1, str3) == 0);
+}
+
+TEST_CASE("strncmp") {
+  constexpr char str1[] = "Hello, world!";
+  constexpr char str2[] = "Hello, everybody!";
+
+  static_assert(proposal::strncmp(str1, str2, 5) == 0);
+  REQUIRE(proposal::strncmp(str1, str2, 5) == 0);
+
+  static_assert(proposal::strncmp(str1, str2, 10) > 0);
+  REQUIRE(proposal::strncmp(str1, str2, 10) > 0);
+
+  static_assert(proposal::strncmp(str2, str1, 10) < 0);
+  REQUIRE(proposal::strncmp(str2, str1, 10) < 0);
 }
